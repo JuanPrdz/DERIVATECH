c14a562c57435fc738c6eab1d919311d504a7dad
Ivan <ivanandresarellano999@gmail.com>
2022-03-19 14:05:24 -0600

    Se hacen mas cambios

--- data.py
+++ data.py
@@ -8,25 +8,28 @@ import pandas as pd
 
 
 # Consultas de Claves indiciduales 
-lista = ["AAPL", "AMZN", "ES=F"]
-lista
-finfn = pd.DataFrame()
+my_dict = finviz.get_stock("AAPL")
+df = pd.DataFrame(list(my_dict.items()),columns = ['column1','column2'])
+indice=df["column1"]
+indice=indice.values
+lista = ["TSLA", "AMZN", "MSFT"]
+finfn = pd.DataFrame(index=indice)
+
 for i in range(len(lista)):
     my_dicto = finviz.get_stock(lista[i])
     dff = pd.DataFrame(list(my_dicto.items()),columns = ['column1','column2'])
-    finfn[i] = dff.column2
-
-indiceD = finviz.get_stock(lista[0])
-indiceDF = pd.DataFrame(list(indiceD.items()),columns = ['column1','column2'])
-finfn = finfn.set_index(indiceDF.column1)
+    finfn[i] = dff['column2'].values
 finfn.columns = lista
 finfn = finfn.T
+
 
 
 # Se genera consultas en https://finviz.com/screener.ashx
 filters = ['sec_financial']
 stock_list= Screener(filters=filters,table='Performance',order='Ticker',signal='New High')
 Data = pd.DataFrame.from_dict(stock_list.data)
+
+
 
 
 
