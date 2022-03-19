6a350b7c2600bc4b67870713cc71eff49c564fc8
Ivan <ivanandresarellano999@gmail.com>
2022-03-19 12:57:08 -0600

    Se hacen cambios en la API

--- data.py
+++ data.py
@@ -4,23 +4,72 @@
 # Conegexión y manejo de información en tiempo real
 from finviz.screener import Screener
 import finviz
-filters = Screener.load_filter_dict()
-
-some_filters = [filters["PEG"]["Under 1"], filters["Exchange"]["AMEX"]]
-stock_list = Screener(filters=some_filters, order="ticker")
-print(stock_list)
+import pandas as pd
 
 
-stock_list = Screener()
-print(type(stock_list))
+# Individual Stocks 
+Lista = ['AAPL','TSLA','MSFT']
+Clave = finviz.get_stock(Lista[0])
+Clave = pd.DataFrame.from_dict(Clave,orient='index')
+for i in range(1,len(Lista)):
+    A = finviz.get_stock(Lista[i])
+    B = pd.DataFrame.from_dict(A,orient='index',columns=(Lista[i]))
+    Clave = Clave.join(B)
+
+B.columns['A']
 
 
-stock = finviz.get_stock('TSLA')
-print(stock)
+# finviz.get_stock(Lista[i])
 
-stock['Price']
+# Clave = finviz.get_stock('AAPL')
+# Clave = pd.DataFrame.from_dict(Clave,orient='index')
 
 
-stock_list = Screener(tickers=['TSLA', 'SQ', 'COIN', 'TWTR'])
 
-a = stock_list.get_charts(period='d', chart_type='c', size='l', ta='1')
+
+
+# some_filters = [filters["PEG"]["Under 1"], filters["Exchange"]["AMEX"]]
+# stock_list = Screener(filters=some_filters, order="ticker")
+# print(stock_list)
+
+
+# stock_list = Screener()
+# print(type(stock_list))
+
+
+# stock = finviz.get_stock('AAPL')
+# print(stock)
+
+# stock['Price']
+
+# stock_list= Screener(table='Performance',order='Ticker',signal='New High',fil)
+# print(stock_list)
+
+# stock_list[3]
+# pd.DataFrame.from_dict(A,orient='index')
+# a = stock_list.get_charts(period='d', chart_type='c', size='l', ta='1')
+
+
+##-----------------------------------------------------------------------------------------------------------------------------------###
+# Indicidual Stocks 
+Clave = finviz.get_stock('AAPL')
+Clave = pd.DataFrame.from_dict(Clave,orient='index')
+
+pd.DataFrame(Clave)
+
+
+
+finviz.get_insider('АAPL')
+
+finviz.get_news('AAPL')
+
+
+finviz.get_analyst_price_targets('AAPL')
+
+
+
+
+
+
+
+
